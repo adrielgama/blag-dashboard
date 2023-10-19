ee7dde1a8bfde93def45c02e112d53855c61f4c3
Adriel Gama <adrielgama@gmail.com>
2023-10-19 16:15:45 -0300

    feat: add vercel analytics and microsoft clarity

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import { inject } from '@vercel/analytics'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './App'
 import './index.css'
+
+inject()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <main className="login">
